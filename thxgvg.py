fb28d1605b29809c41b1713f73ebb6000c830fd3
Patricia Decker <patriciasquared@gmail.com>
2015-12-05 22:09:48 -0800

    is_prime added

--- thxgvg.py
+++ thxgvg.py
@@ -23,6 +23,42 @@ def is_palindrome(somestr):
             return False
     return True
 
+
+def is_prime(num):
+    """Return True if integer num is a prime number.
+
+    >>> is_prime(0)
+    False
+
+    >>> is_prime(1)
+    False
+
+    >>> is_prime(2)
+    True
+
+    >>> is_prime(3)
+    True
+
+    >>> is_prime(4)
+    False
+
+    >>> is_prime(11)
+    True
+
+    >>> is_prime(999)
+    False
+    """
+    # a prime number >= 2
+    if num < 2:
+        return False
+
+    # if mod any number larger than 2 but smaller than the number == 0
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
+
+
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
