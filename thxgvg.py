75d9c29570504b526c21bf6fba0a6bce64c71f8c
Patricia Decker <patriciasquared@gmail.com>
2015-12-05 22:34:15 -0800

    started sort_ab

--- thxgvg.py
+++ thxgvg.py
@@ -1,20 +1,20 @@
 def is_palindrome(somestr):
     """Return True if string somestr is a palindrome.
 
-    >>> is_palindrome("a")
-    True
+        >>> is_palindrome("a")
+        True
 
-    >>> is_palindrome("noon")
-    True
+        >>> is_palindrome("noon")
+        True
 
-    >>> is_palindrome("racecar")
-    True
+        >>> is_palindrome("racecar")
+        True
 
-    >>> is_palindrome("porcupine")
-    False
+        >>> is_palindrome("porcupine")
+        False
 
-    >>> is_palindrome("Racecar")
-    False
+        >>> is_palindrome("Racecar")
+        False
     """
     # number of steps required at most (len(somestr) / 2)
     for i in range(len(somestr)):
@@ -27,26 +27,26 @@ def is_palindrome(somestr):
 def is_prime(num):
     """Return True if integer num is a prime number.
 
-    >>> is_prime(0)
-    False
+        >>> is_prime(0)
+        False
 
-    >>> is_prime(1)
-    False
+        >>> is_prime(1)
+        False
 
-    >>> is_prime(2)
-    True
+        >>> is_prime(2)
+        True
 
-    >>> is_prime(3)
-    True
+        >>> is_prime(3)
+        True
 
-    >>> is_prime(4)
-    False
+        >>> is_prime(4)
+        False
 
-    >>> is_prime(11)
-    True
+        >>> is_prime(11)
+        True
 
-    >>> is_prime(999)
-    False
+        >>> is_prime(999)
+        False
     """
     # a prime number >= 2
     if num < 2:
@@ -66,14 +66,14 @@ def lucky_numbers(n):
     the same number twice. You can trust that this function will never be called
     with n < 0 or n > 10.
 
-    # >>> lucky_numbers(2)
-    # [3, 7]
+        # >>> lucky_numbers(2)
+        # [3, 7]
 
-    >>> lucky_numbers(0)
-    []
+        >>> lucky_numbers(0)
+        []
 
-    >>> sorted(lucky_numbers(10))
-    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        >>> sorted(lucky_numbers(10))
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     """
     if n == 0:
         return []
@@ -92,6 +92,55 @@ def lucky_numbers(n):
     return lucky_nums
 
 
+def sort_ab(a, b):
+    """Given already-sorted lists, `a` and `b`, return sorted list of both.
+
+    You may not use sorted() or .sort().
+
+    Check edge cases of empty lists:
+
+        >>> sort_ab([], [])
+        []
+
+        >>> sort_ab([1, 2,3], [])
+        [1, 2, 3]
+
+        >>> sort_ab([], [1, 2, 3])
+        [1, 2, 3]
+
+    Check:
+
+        >>> sort_ab([1, 3, 5, 7], [2, 6, 8, 10])
+        [1, 2, 3, 5, 6, 7, 8, 10]
+    """
+    merged_list = []
+
+    # find shorter list
+    if len(a) < len(b):
+        shorter_list = a[:]
+        longer_list = b[:]
+    else:
+        shorter_list = b[:]
+        longer_list = a[:]
+
+    # iterate on list a, checking if next value in list b is smaller
+    for val_a in shorter_list:
+        for val_b in longer_list:
+            print "this is val_a", val_a
+            print "this is val_b", val_b
+            if val_a < val_b:
+                merged_list.append(val_a)
+                shorter_list.pop(val_a)
+            else:
+                merged_list.append(val_b)
+                longer_list.pop(val_b)
+
+    # add remaining items to list
+    merged_list.extend(longer_list)
+
+    return merged_list
+
+
 if __name__ == "__main__":
     import doctest
-    doctest.testmod()
+    # doctest.testmod()
